2d8ae9f6cc82d9f59ab6af4510b9373a832d759d
Lamaproinfinite <nguyentunglambuncha@gmail.com>
2026-07-18 17:57:32 +0700

    rename the backend script to main.py as industry standard

--- config.py
+++ config.py
@@ -5,10 +5,11 @@ class Settings(BaseSettings):
     # this is the default variable if it doesnt find any variable named 'ENVIRONMENT'
 
     GROQ_API_KEY : str 
-    #this has no fallback default, if couldnt find then the app crashes
+    #this has no fallback default, if couldnt find the var then the app crashes
 
     model_config = SettingsConfigDict(env_file='.env')
-    #this tell the model what to look for
+    #model_config is an fixed built in parameter, must write it like that
+    #this tell the model what file to look for
 
 env_config = Settings()
 #this is the object that contains all our sensitive info from the .env file'
